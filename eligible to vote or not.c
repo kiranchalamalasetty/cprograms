1ec30d7972d204b3d7a6b6affb2da257406791ae
kiranchalamalasetty <kiranchalamalasetty2@gmail.com>
2024-10-21 12:27:11 +0530

    Create eligible to vote or not.c

---
+++ eligible to vote or not.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int age;
+    printf("enter the age:");
+    scanf("%d",&age);
+    if(age>=18)
+    printf("YOU ARE ELIGIBLE TO VOTE:");
+    else
+    printf("YOUR ARE NOT ELIGIBLE  TO VOTE" );
+}
